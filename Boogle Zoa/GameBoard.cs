1a46a5557eba19ec4be655646789ae206333ffd0
NoaWrt <noa.warot@edu.devinci.fr>
2024-11-07 09:39:18 +0100

    Version 4 : Ajout du GameBoard

---
+++ Boogle Zoa/GameBoard.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Boogle_Zoa
+{
+    internal class GameBoard
+    {
+        private Dice[] dices;
+        private char[] visibleLetters;
+        private const int size = 16;
+
+        public GameBoard(Dice[] d)
+        {
+            dices = d;
+            for(int i = 0;i<dices.Length;i++)
+            {
+                dices[i].visibleLetter = visibleLetters[i];
+            }
+        }
+        
+        public string toString()
+        {
+            string description = "";
+
+            int side = sqrt(size);
+            int n = 0;
+
+            for (int i = 0; i < side; i++)
+            {
+                for (int j = 0; j < side; j++)
+                {
+                    description += visibleLetters[n] + " ";
+                    n++;
+                }
+                description += "\n";
+            }
+            return description;
+        }
+
+        public bool GameBoard_Test(string mot)
+        {
+
+        }
+    }
+}
