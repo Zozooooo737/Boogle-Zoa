03e309020a18d4ef233dd02ef41573c3890ff4b1
NoaWrt <noa.warot@edu.devinci.fr>
2024-11-07 08:54:51 +0100

    Version 3 : Test Git de Noa

--- Boogle Zoa/Program.cs
+++ Boogle Zoa/Program.cs
@@ -7,6 +7,8 @@
             Console.WriteLine("Hello, World!");
 
             Console.WriteLine("Test, depuis mon petit ordinateur");
+
+            Console.WriteLine("Test de Noa, depuis mon petit ordinateur de noa");
         }
     }
 }
