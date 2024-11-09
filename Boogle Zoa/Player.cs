1d62d5870de29f0067dbb43c314420b9f4e49282
Zozooooo737 <enzo.bottura137@gmail.com>
2024-11-09 13:08:48 +0100

    Version 15 : Reformulation des commentaires en XML

--- Boogle Zoa/Player.cs
+++ Boogle Zoa/Player.cs
@@ -1,63 +1,82 @@
 ﻿namespace Boogle_Zoa
 {
+    /// <summary>
+    /// Représente un joueur qui dispose d'un nom (<see cref="name"/>) ainsi que d'un score (<see cref="score"/>) et d'une liste des mots qui l'a trouvés (<see cref="wordsFound"/>), respectivement initialisé à 0 et par une liste vide.
+    /// </summary>
     internal class Player
     {
-        #region Attributes
-
-        private string name;                            // Nom du Joueur.
-        private int score;                              // Score du Joueur.
-        private List<string> wordsFound;                // Liste des mots trouvées par le Joueur.
-
-        #endregion Attributes
+        private string name;
+        private int score;
+        private List<string> wordsFound;
 
 
-        #region Constructor
 
-        public Player(string n)                         // Paramètre : Nom du Joueur.
+        /// <summary>
+        /// Crée un joueur à partir d'un nom (<see cref="name"/>).
+        /// </summary>
+        /// <param name="n">Nom du Joueur</param>
+        public Player(string name)
         {
-            this.name = n;
-            score = 0;                                  // On initialise son score à 0.
-            wordsFound = new List<string>() { };        // On initialise sa liste de mot avec une List vide.
+            this.name = name;
+            score = 0;
+            wordsFound = new List<string>() { };   
         }
 
-        #endregion Constructor
 
 
-        #region Properties
-
-        public int Score                                // Propriété à revoir, pas encore utilisé.
+        /// <summary>
+        /// Renvoie le (<see cref="score"/>) du Joueur.
+        /// </summary>
+        public int Score
         {
             get { return score; }
         }
 
-        #endregion Properties
 
-
-        #region Methods
-
-        public bool Contain(string w)                   // Méthode qui regarde si le mot w est présent dans la liste des mots trouvés par le Joueur.
+        /// <summary>
+        /// Vérifie si un mot donné (<paramref name="word"/>) est présent dans la liste des mots trouvés par le joueur (<see cref="wordsFound"/>).
+        /// </summary>
+        /// <param name="word">Le mot à rechercher dans la liste des mots trouvés par le joueur.</param>
+        /// <returns>
+        /// <c>true</c> si le mot est présent dans la liste des mots trouvés par le joueur ; sinon, <c>false</c>.
+        /// </returns>
+        public bool Contain(string word)
         {
-            return wordsFound.Contains(w);
+            return wordsFound.Contains(word);
         }
 
 
-        public void AddWord(string w, Dictionary<char, int[]> lettersInformation)          // Méthode qui prends en paramètres un mot et les informations concernant les lettres (nombre, poids).
+        /// <summary>
+        /// Ajoute un mot <paramref name="word"/> à la liste des mots trouvés (<see cref="wordsFound"/>) et met à jour le score du joueur (<see cref="score"/>). Le score est calculé en additionnant
+        /// le poids de chaque lettre du mot, selon les valeurs présentes dans le dictionnaire <paramref name="lettersInformation"/>.
+        /// </summary>
+        /// <param name="word">Le mot à ajouter à la liste des mots trouvés</param>
+        /// <param name="lettersInformation">Un dictionnaire où chaque clé est une lettre et chaque valeur est un tableau de taille 2 représentant le poids et le nombre de cette lettre.</param>
+        /// <remarks>
+        /// Avant d'ajouter le mot <paramref name="word"/>, on vérifie qu'il n'est pas déjà présent dans la liste <see cref="wordsFound"/>.
+        /// Il est supposé que <paramref name="word"/> existe dans le dictionnaire de la langue concernée
+        /// </remarks>
+        public void AddWord(string word, Dictionary<char, int[]> lettersInformation)
         {
-            if (!Contain(w))                            // On vérifie que le mot n'est pas déjà présent dans la liste des mots trouvés par le Joueur
+            if (!Contain(word))
             {
-                wordsFound.Add(w);                      // On ajoute ce mot dans sa liste /!\ On considère que le mot est présent dans le dictionnaire, à revoir
+                wordsFound.Add(word);
                    
-                foreach(char c in w)
+                foreach(char c in word)
                 {
-                    score += lettersInformation[w[c]][0];                                  // Grâce à lettersInformations, on obtient le poids de chaque lettre, qu'on ajoute au score du Joueur.
+                    score += lettersInformation[word[c]][0];
                 }
             }
         }
 
 
-        public string toString()                        // Méthode qui renvoie la description du Joueur.
+        /// <summary>
+        /// Renvoie une chaîne de caractère <c>string</c> qui décrit le joueur (<see cref="Player"/>) avec son nom (<see cref="name"/>), son score (<see cref="score"/>) et sa liste des mots qu'il a trouvé (<see cref="wordsFound"/>).
+        /// </summary>
+        /// <returns>Renvoie une chaîne de caractère <c>string</c> déjà structurée.</returns>
+        public string toString()
         {
-            string description = $"Joueur : {name} \n" +                                   // On utilise l'interpolation de chaînes pour faciliter l'insertion des attributs dans le string
+            string description = $"Joueur : {name} \n" +
                                  $"Score : {score} \n" +
                                   "Mots trouvés : ";
 
@@ -67,7 +86,5 @@
             }
             return description;
         }
-
-        #endregion Methods
     }
 }
--- Boogle Zoa/Program.cs
+++ Boogle Zoa/Program.cs
@@ -8,7 +8,7 @@
 
             Random random = new Random();
 
-            Dictionary<char, int[]> dictionnaire = new Dictionary<char, int[]>
+            Dictionary<char, int[]> lettersInforamtion = new Dictionary<char, int[]>
             {
                 {'A', new int[] {1, 9}},
                 {'B', new int[] {3, 2}},
@@ -38,22 +38,22 @@
                 {'Z', new int[] {10, 1}}
             }; // A FAIRE
 
-            Dice A1 = new Dice(random, dictionnaire);
-            Dice A2 = new Dice(random, dictionnaire);
-            Dice A3 = new Dice(random, dictionnaire);
-            Dice A4 = new Dice(random, dictionnaire);
-            Dice B1 = new Dice(random, dictionnaire);
-            Dice B2 = new Dice(random, dictionnaire);
-            Dice B3 = new Dice(random, dictionnaire);
-            Dice B4 = new Dice(random, dictionnaire);
-            Dice C1 = new Dice(random, dictionnaire);
-            Dice C2 = new Dice(random, dictionnaire);
-            Dice C3 = new Dice(random, dictionnaire);
-            Dice C4 = new Dice(random, dictionnaire);
-            Dice D1 = new Dice(random, dictionnaire);
-            Dice D2 = new Dice(random, dictionnaire);
-            Dice D3 = new Dice(random, dictionnaire);
-            Dice D4 = new Dice(random, dictionnaire);
+            Dice A1 = new Dice(random, lettersInforamtion);
+            Dice A2 = new Dice(random, lettersInforamtion);
+            Dice A3 = new Dice(random, lettersInforamtion);
+            Dice A4 = new Dice(random, lettersInforamtion);
+            Dice B1 = new Dice(random, lettersInforamtion);
+            Dice B2 = new Dice(random, lettersInforamtion);
+            Dice B3 = new Dice(random, lettersInforamtion);
+            Dice B4 = new Dice(random, lettersInforamtion);
+            Dice C1 = new Dice(random, lettersInforamtion);
+            Dice C2 = new Dice(random, lettersInforamtion);
+            Dice C3 = new Dice(random, lettersInforamtion);
+            Dice C4 = new Dice(random, lettersInforamtion);
+            Dice D1 = new Dice(random, lettersInforamtion);
+            Dice D2 = new Dice(random, lettersInforamtion);
+            Dice D3 = new Dice(random, lettersInforamtion);
+            Dice D4 = new Dice(random, lettersInforamtion);
 
             Dice[] listOfDice = new Dice[16] { A1, A2, A3, A4, B1, B2, B3, B4, C1, C2, C3, C4, D1, D2, D3, D4 };
 
@@ -66,6 +66,13 @@
             Console.WriteLine(Dico.toString());
 
             Console.WriteLine(Dico.RechDichoRecursif("Table"));
+            Player noa = new Player("noa");
+            Player p = new Player("zozo");
+            Console.WriteLine(p.Score);
+            p.Contain("cc");
+            p.AddWord("cc", lettersInforamtion);
+            Console.WriteLine(p.toString());
+            p.toString();
         }
     }
 }
